5733bca572aee871f72381af66d62a5cc06b446a
MahnoorGhias <147559332+MahnoorGhias@users.noreply.github.com>
2024-02-29 11:02:07 -0600

    Update test_contactlist.py

--- Problem#6/test_contactlist.py
+++ Problem#6/test_contactlist.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from mai import process_user_contacts
+from main import process_user_contacts
 
 class TestProcessUserContacts(unittest.TestCase):
 
